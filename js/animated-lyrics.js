e6f05fcf139da2399715fed245de43d5d98edf6e
Gedion <gedion@Gedions-iMac.local>
2016-11-12 08:03:52 -0600

    Refactored index.svg

--- js/animated-lyrics.js
+++ js/animated-lyrics.js
@@ -1,9 +1,3 @@
-/** =======================================================================
- *  Vincent Hardy
- *  License terms: see svg-wow.org
- *  CC0 http://creativecommons.org/publicdomain/zero/1.0/
- *  ======================================================================= */
-
 var sw = sw || {};
 
 sw.animatedLyrics = sw.animatedLyrics || {};
@@ -37,7 +31,6 @@ sw.animatedLyrics.init = function () {
     var s;
     var startTime;
     var nextBeat = -1;
-    var attributionsAnim;
     var startBeat;
 
     var beatTween = {
@@ -72,8 +65,7 @@ sw.animatedLyrics.init = function () {
 
     t.appendChild(tp);
         tp.appendChild(document.createTextNode(l[1].toLowerCase()));
-        tp.setAttribute("startOffset", (25 + 50 * Math.random()) + "%");
-    // tp.setAttribute("text-anchor", "middle");
+        tp.setAttribute("startOffset", (250 + 500 * Math.random()) + "%");
         tp.setAttributeNS(sw.xlinkNS, "href", "#linePath");
 
         if (i < nLines - 1) {
@@ -139,12 +131,12 @@ sw.animatedLyrics.init = function () {
             fadeIn.onEnd(speedFadeOut);
             speedFadeOut.onEnd(getStopHandler(t, songText));
         }
-        
+
         fadeIn.offset = l[0];
         fadeIn.deltaOffset = fadeIn.offset - previousOffset;
         previousOffset = fadeIn.offset;
         playList.push(fadeIn);
-        
+
         t.setAttribute('y', (-100 + Math.random()*200));
         t.setAttribute("transform", "scale(0.2,0.2)")
     }
@@ -195,8 +187,6 @@ sw.animatedLyrics.init = function () {
         startBeat.onEnd(getStopHandler(beat, beats));
     }
 
-    //buildAttributions();
-
     function getStartHandler(t, c) {
         return function () {
             c.appendChild(t);
@@ -209,29 +199,13 @@ sw.animatedLyrics.init = function () {
         };
     }
 
-    function getSpeedFadeOutHandler(previousFadeOut, 
-                                    previousSpeedFadeOut,
-                                    previousFadeIn) {
-        return function () {
-            // Check if the previousFadeIn is still running
-            if (previousFadeIn.isAnimated()) {
-                previousFadeIn.stop();
-                previousSpeedFadeOut.animate();
-            } else if (previousFadeOut.isAnimated()) {
-                previousFadeOut.stop();
-                previousSpeedFadeOut.animate();
-            }
-        }
-    }
-
-
     function computeOffset (offset) {
         var seconds = secondsRegxp.exec(offset);
         var minutes;
         var s, m;
         if (seconds !== null) {
             s = seconds[1];
-            return parseFloat(s);            
+            return parseFloat(s);
         } else {
             minutes = minutesRegxp.exec(offset);
             if (minutes !== null) {
@@ -276,7 +250,6 @@ sw.animatedLyrics.init = function () {
             nextBeat++;
             if (nextBeat >= song.beats.length) {
                 nextBeat = -1;
-                //showCast.animate();
             }
         }
     }
@@ -313,258 +286,10 @@ sw.animatedLyrics.init = function () {
         }
     }
 
-    function buildAttributions () {
-        var attributions = song.attributions;
-        var attributionLines;
-        var attributionsGroup = $("attributionsGroup");
-        var curLine = -0.5;
-        var curGroup;
-        var prevFadeOut;
-        var firstFadeIn;
-        var fadeIn;
-        var fadeOut;
-        var show;
-        var lastFadeOut;
-
-        for (var p in attributions) {
-            if (attributions.hasOwnProperty(p) === true) {
-                attributionLines = attributions[p].split("\n");
-                curGroup = attributionsGroup.loadContent({
-                    tag: "g"
-                });
-
-                curGroup.loadContent({
-                    tag: "text",
-                    "class": "attributionType",
-                    y: (curLine++ * 1.1) + "em",
-                    children: [p]
-                });
-                
-                for (var i = 0; i < attributionLines.length; i++) {
-                    curGroup.loadContent({
-                        tag: "text",
-                        "class": "attributionValue",
-                        y: (curLine++ * 1.1) + "em",
-                        children: [attributionLines[i]]
-                    });
-                }
-
-                fadeIn = new Animate(attributionsGroup, {
-                    txf: {
-                        template: "scale(1, #sy)",
-                        from: {sy: 0.01},
-                        to: {sy: 1}
-                    }
-                }, 2, yui.util.Easing.elasticOut);
-
-                show = new Animate(attributionsGroup, {
-                    txf: {
-                        from: {sy: 1},
-                        to: {sy: 1},
-                        template: "scale(1, 1)"
-                    }
-                }, 1, undefined);
-
-                fadeOut = new Animate(attributionsGroup, {
-                    txf: {
-                        from: {sy: 1},
-                        tp: {sy: 0.01},
-                        template: "scale(1, #sy)"
-                    }
-                }, 0.25, yui.util.Easing.backIn);
-
-                fadeIn.onBegin(curGroup.getShowHandler());
-                fadeIn.onEnd(show);
-                show.onEnd(fadeOut);
-                fadeOut.onEnd(curGroup.getHideHandler());
-                lastFadeOut = fadeOut;
-                
-                if (firstFadeIn === undefined) {
-                    firstFadeIn = fadeIn;
-                } else {
-                    prevFadeOut.onEnd(fadeIn);
-                }
-
-                prevFadeOut = fadeOut;
-                curLine = -0.5;
-            }
-        }
-
-        lastFadeOut.onEnd(attributionsGroup.getHideHandler());
-
-        // Add a background rectangle
-        var bbox = attributionsGroup.getBBox();
-        var firstChild = attributionsGroup.firstElementChild;
-        var padding = 10;
-        var bkg = attributionsGroup.loadContent({
-            tag: "rect",
-            x: -150,
-            y: bbox.y - padding,
-            width: 300,
-            height: bbox.height + 2 * padding,
-            fill: "url(#attributionsFill)",
-            "fill-opacity": 0.5,
-            stroke: "url(#attributionsStroke)",
-            "stroke-opacity": 0.5,
-            "stroke-width": 1,
-            rx: 5,
-            ry: 5,
-            display: "none"
-        }, firstChild);
-
-        var c = firstChild;
-        while (c !== null) {
-            c.setAttribute("display", "none");
-            c = c.nextElementSibling;
-        }
-
-        firstFadeIn.onBegin(bkg.getShowHandler());
-        attributionsAnim = firstFadeIn;
-    }
-/*
-
-    yui.util.Event.addListener("credits", "click", play);
-
-    var showCredits = new Animate("credits", {
-            txf: {
-                to: {ty: 250, r: 0},
-                from: {r: 180},
-                template: 'translate(400,#ty) rotate(#r)'
-            }
-        }, 1.2, yui.util.Easing.elasticOut);
-    var showCredits2 = new Animate("credits2", {
-            txf: {
-                to: {ty: 320, r: 0},
-                from: {r: -180},
-                template: 'translate(400,#ty) rotate(#r)'
-            }
-        }, 1, yui.util.Easing.elasticOut);
-    var start = showCredits.getStartHandler();
-
-*/
-    var started = false;
-
     yui.util.Event.addListener(document, "click", function () {
             if (startTime === undefined) {
                 startTime = (new Date()).getTime();
-                play();
-            } else {
-                console.log((new Date()).getTime() - startTime);
             }
+            play();
     });
-
-/*    showCredits.onBegin(showCredits2);
-    
-    var hideCredits = new Animate("credits", {
-            txf: {
-                to: {ty: 600},
-                template: 'translate(400,#ty)'
-            }
-        }, 0.6, yui.util.Easing.backIn);
-    var hideCredits2 = new Animate("credits2", {
-            txf: {
-                to: {ty: 600},
-                template: 'translate(400,#ty)'
-            }
-        }, 0.5, yui.util.Easing.backIn);
-
-    var hideCast = new Animate("cast", {
-        "fill-opacity": {
-            from: 0.75,
-            to: 0
-        },
-        "opacity": {
-            from: 0.75,
-            to: 0
-        }
-    }, 3);
-
-    var showCast = new Animate("cast", {
-        "fill-opacity": {from: 0, to: 0.75},
-        opacity: {from: 0, to: 0.75}}, 3);
-
-    hideCredits.onBegin(hideCredits2);
-    hideCredits.onBegin(hideCast);
-    showCast.onBegin(attributionsAnim);
-    showCast.onBegin($("cast").getShowHandler());
-    hideCast.onEnd($("cast").getHideHandler());
-*/
-}
-
-// Wait for the audio to be ready before allowing the demo to start.
-// =============================================================================
-
-function onAudioReady () {
-/*    animateLoadingAudio.stop();
-    audioLoaded = true;
-    $('instructions').setAttribute("display", "inline");*/
-
-}
-
-function initLoadingAudio () {
-    /*hideLoadingAudio =
-        new sw.animation.Animate("loadingAudioMessage", {
-            opacity: {
-                to: 0
-            }
-        },
-        0.25);
-
-    showInstructions =
-        new sw.animation.Animate("instructions", {
-            opacity: {
-                to: 1
-            }
-        },
-        0.25);
-
-    animateLoadingAudio =
-        new sw.animation.Animate("loadingAudio", {
-            txf: {
-                from: {r: 0},
-                to: {r: 360},
-                template: "rotate(#r)"
-            }
-        },
-        2, undefined, "rotate(#r)");
-
-    animateLoadingAudio.animate();
-
-    animateLoadingAudio.onEnd(function () {
-        if (audioLoaded === false) {
-            animateLoadingAudio.animate();
-        }
-    });
-*/
-}
-
-// Safari 5 Work around alert
-// =============================================================================
-// Work around issue in Safari 5 where an image in a pattern does not update
-// the pattern after it is loaded. So if the image is not in the cache, the
-// pattern does not display and is incomplete.
-
-// The following event handler gets called when the background rectangle has 
-// loaded and is ready to be updated.
-// =============================================================================
-function onBackgroundLoad() {
-    var backgroundImage = new Image();
-
-    backgroundImage.onload = function (evt) {
-        var imagePattern = $('imagePattern');
-        var c = imagePattern.lastChild;
-        while (c !== null) {
-            imagePattern.removeChild(c);
-            c = imagePattern.lastChild;
-        }
-        imagePattern.loadContent({
-            tag: "image",
-            "xlink:href": "background.png",
-            width: 800,
-            height: 550
-        });
-        $('bgFill').setAttribute("fill", "url(#imagePattern)");
-    };
-
-    backgroundImage.src = "background.png";
 }
